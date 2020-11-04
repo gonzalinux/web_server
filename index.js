91cea9aad874ee10c9a4626edb22fef41cb0c60d
gonzalinux <gonzaleonrey@hotmail.com>
2020-11-04 13:56:36 +0100

    adada

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const http=require("http");
 const fs=require("fs")
 //const host="132.145.20.242";
 //const host="localhost";
-const port=8000;
+const port=80;
 let contents=fs.readFileSync("index.html" )
+let archivo=fs.readFileSync("ips.txt")
+archivo=archivo.toString().split("\n");
+console.log(archivo)
 
 
     const requestListener=function(req,res){
 
-
-            console.log(req.connection.remoteAddress);
+        let ip=req.connection.remoteAddress;
+        for(let i=0;i<archivo.length;i++)
+            if(archivo[i]!=ip) {
+            console.log("ada")
+                fs.writeFileSync("ips.txt", archivo + "\n" + ip)
+            }
             res.setHeader("Content_Type","text/html");
             res.writeHead(200);
             res.end(contents);
